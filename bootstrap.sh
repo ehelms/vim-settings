59e2f1591281b8891154602fc51c69a7006bbb17
Eric D. Helms <ericdhelms@gmail.com>
2013-02-24 15:56:50 -0500

    Adds RVM directive

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,8 @@ cp .bashrc ../
 cp screen/.screenrc ../
 cp vim/.vimrc ../
 cp -rf vim/ ../.vim
-source ../.bashrc
 git config --global user.name "Eric D. Helms"
 git config --global user.email ericdhelms@gmail.com
 mkdir ../workspace
+\curl -L https://get.rvm.io | bash -s stable --ruby
+source ~/.bashrc
