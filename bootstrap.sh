8abbe3c4f865792b71e80207843cdb0406023f3e
Eric D. Helms <ericdhelms@gmail.com>
2013-11-28 19:20:35 -0500

    Changing bootstrap to use symlinks.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,16 @@
+CURR_DIR=`pwd`
+
 sudo yum install -y ruby-devel git screen vim
 sudo yum groupinstall -y "Development Tools"
-cp .rdebugrc ../
-cp .bashrc ../
-cp screen/.screenrc ../
-cp vim/.vimrc ../
-cp -rf vim/ ../.vim
+ln -s $CURR_DIR/.rdebugrc ~/.rdebugrc
+ln -s $CURR_DIR/.bashrc ~/.bashrc
+ln -s $CURR_DIR/screen/.screenrc ~/.screenrc
+ln -s $CURR_DIR/vim/.vimrc ~/.vimrc
+ln -s $CURR_DIR/vim/.vundle ~/.vundle
+ln -s $CURR_DIR/vim ~/.vim
+vim +BundleInstall +qall
 git config --global user.name "Eric D. Helms"
 git config --global user.email ericdhelms@gmail.com
-mkdir ../workspace
+mkdir ~/workspace
 \curl -L https://get.rvm.io | bash -s stable --ruby
 source ~/.bashrc
