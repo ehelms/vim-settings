0229517a2ff43724385534a60c3e1ea1d739a2fd
Eric D. Helms <ericdhelms@gmail.com>
2018-03-28 19:34:55 -0400

    Convert all to bootstrap.yml

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,4 @@ sudo yum -y install ansible
 
 ansible-playbook bootstrap.yml --ask-sudo-pass
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-vim +PlugInstall +qall
-
 source ~/.bashrc
