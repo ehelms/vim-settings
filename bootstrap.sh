28a77b159684920df1f05b5c6dc274ac8a886e96
Eric D. Helms <ericdhelms@gmail.com>
2020-11-03 21:04:51 +0000

    Update for EL8

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+sudo yum -y install centos-release-ansible-29
 sudo yum -y install ansible
 
-ansible-playbook bootstrap.yml --ask-sudo-pass
+ansible-playbook bootstrap.yml
 
 source ~/.bashrc
